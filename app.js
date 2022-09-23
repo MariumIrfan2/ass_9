ccf0dde416a943fed454be474ef08c069028046a
MariumIrfan2 <111229052+MariumIrfan2@users.noreply.github.com>
2022-09-23 15:58:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// changing case pdf
+
+// task 1
+// var input = prompt("enter your name");
+// var input = input.toUpperCase();
+// document.write(input); 
+
+// task 2
+// var input = prompt("enter your name");
+// var fChar = input.slice(0, 1);
+// var fChar = fChar.toUpperCase();
+
+// var otherChar = input.slice(2);
+// var otherChar = otherChar.toLowerCase();
+// var output = fChar + otherChar;
+
+// document.write(output);
+
+// Task 3
+// var mobile = prompt("enter your Favourite Mobile phone Model");
+// var x = mobile.length;
+// document.write(x);
+
+// task 4
+var input = prompt('enter your name');
+var lastChar = input.pop();
+document.write(lastChar);
+
+// Finding Segments
+// task 5
+
+
